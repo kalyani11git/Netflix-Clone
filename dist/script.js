d6a22209e9029b5e460612d6586e38cb72a67ec4
kalyani Mali <kalyanimali2003@gmail.com>
2024-07-22 16:18:34 +0530

    Second Commit

---
+++ dist/script.js
@@ -0,0 +1,111 @@
+let state;
+
+    let plus = document.getElementById("plus");
+    plus.addEventListener("click",()=>{
+        let hideDiv = document.getElementById("hidediv");
+        console.log(hideDiv);
+        if(state){
+        hideDiv.style.display='none';
+        state=false;
+        }
+        else{
+            hideDiv.style.display='block';
+            state = true;
+        }
+    })
+
+
+
+    let state2;
+
+    let plus2 = document.getElementById("plus2");
+    plus2.addEventListener("click",()=>{
+        let hideDiv2 = document.getElementById("hidediv2");
+        console.log(hideDiv2);
+        if(state2){
+        hideDiv2.style.display='none';
+        state2=false;
+        }
+        else{
+            hideDiv2.style.display='block';
+            state2 = true;
+        }
+    })
+
+
+
+    let state3;
+
+    let plus3 = document.getElementById("plus3");
+    plus3.addEventListener("click",()=>{
+        let hideDiv3 = document.getElementById("hidediv3");
+        console.log(hideDiv3);
+        if(state3){
+        hideDiv3.style.display='none';
+        state3=false;
+        }
+        else{
+            hideDiv3.style.display='block';
+            state3 = true;
+        }
+    })
+
+
+
+
+    let state4;
+
+    let plus4 = document.getElementById("plus4");
+    plus4.addEventListener("click",()=>{
+        let hideDiv4 = document.getElementById("hidediv4");
+        console.log(hideDiv4);
+        if(state4){
+        hideDiv4.style.display='none';
+        state4=false;
+        }
+        else{
+            hideDiv4.style.display='block';
+            state4 = true;
+        }
+    })
+
+
+
+
+    let state5;
+
+    let plus5 = document.getElementById("plus5");
+    plus5.addEventListener("click",()=>{
+        let hideDiv5 = document.getElementById("hidediv5");
+        console.log(hideDiv5);
+        if(state5){
+        hideDiv5.style.display='none';
+        state5=false;
+        }
+        else{
+            hideDiv5.style.display='block';
+            state5 = true;
+        }
+    })
+
+
+
+
+
+
+    let state6;
+
+    let plus6 = document.getElementById("plus6");
+    plus6.addEventListener("click",()=>{
+        let hideDiv6 = document.getElementById("hidediv6");
+        console.log(hideDiv6);
+        if(state6){
+        hideDiv6.style.display='none';
+        state6=false;
+        }
+        else{
+            hideDiv6.style.display='block';
+            state6 = true;
+        }
+    })
+    
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+   "./dist/index.html",
+   './dist/srcipt.js',
+   "./public/*.{jpg,png}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+   
+  ],
+}
+
