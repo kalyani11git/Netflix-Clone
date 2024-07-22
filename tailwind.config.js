0e56c7a1efe7741d7a9958e0ee3a723ba430b497
kalyani Mali <kalyanimali2003@gmail.com>
2024-07-22 16:47:05 +0530

    third commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-   "./dist/index.html",
-   './dist/srcipt.js',
+   "./index.html",
+   './srcipt.js',
    "./public/*.{jpg,png}"
   ],
   theme: {
